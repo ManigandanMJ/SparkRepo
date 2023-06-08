ff01b819f7ecc7478cb8a0b0d2908200ff1ae86d
ManigandanMJ <manigandan.mj@diggibyte.com>
2023-06-08 17:50:28 +0530

    updated functions

--- src/Assignment_1/driver.py
+++ src/Assignment_1/driver.py
@@ -12,10 +12,12 @@ dataframe_join(user_df,transact_df)
 combine_df = dataframe_join(user_df,transact_df)
 
 #count of location
-location_count(combine_df).show()
+location_count(combine_df)
+#combine_df.show()
 
 #product bought
-product_user(user_df,transact_df)
+product_user(combine_df)
+
 
 #total spend of products
-total_spend(user_df,transact_df)
+total_spend(combine_df)
