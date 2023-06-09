fcb32cb38ec6f53f34d92d158bb678bcd8627460
ManigandanMJ <manigandan.mj@diggibyte.com>
2023-06-09 18:59:11 +0530

    updated parameters for functions

--- src/Assignment_1/driver.py
+++ src/Assignment_1/driver.py
@@ -8,8 +8,11 @@ user_df = dataframe_read(spark)
 transact_df = dataframe_read_transact(spark)
 
 #joining dataframes
-dataframe_join(user_df,transact_df)
-combine_df = dataframe_join(user_df,transact_df)
+user_df_col = "user_id"
+transaction_df_col = "userid"
+join_type = "inner"
+combine_df = dataframe_join(user_df,transact_df,user_df_col,transaction_df_col,join_type)
+combine_df.show()
 
 #count of location
 location_count(combine_df)
